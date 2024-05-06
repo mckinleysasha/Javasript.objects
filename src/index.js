e5019bf2d4dda00e1305b71415aaf9ab02e8cf39
mckinleysasha <134669703+mckinleysasha@users.noreply.github.com>
2024-05-06 03:54:19 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+let weather = {};
+
+console.log(weather);
+ 
+let weather = {
+   temperature: 46°F
+   humidity: 85%
+}
+
+console.log(weather.temperature)
+console.log(weather.humidity)
+
+//challenge 4
+weather.windspeed = 9
+console.log(weather.windSpeed)
+
+//challenge 5
+console.log(weather["temperature"]);
+console.log(weather["windSpeed"]);
+console.log(weather["humidity"]);
+
+//challenge 6
+
+// Challenge 6
+let forecast = [
+{
+      day: "Monday",
+      temp: 54°
+    },
+{
+      day: "Tuesday",
+      temp: 58°
+    },
+{
+      day: "Wednesday",
+      temp: 63°
+    },
+{
+      day: "Thursday",
+      temp: 72°
+    },
+{
+      day: "Friday",
+      temp: 80°
+    }
+{
+        day: "Saturday",
+        temp: 81°
+      },
+{
+    day: "Sunday",
+        temp: 77°
+    }
+  ];
+  console.log(forecast);
+  
+
